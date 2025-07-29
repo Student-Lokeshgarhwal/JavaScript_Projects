e8089d6f432f15614f62b28a6165b42cde111782
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2025-07-29 23:05:46 +0530

    fix final bug

--- TicTacToeGame/script.js
+++ TicTacToeGame/script.js
@@ -1,8 +1,7 @@
-
 console.log("js")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
+let music = new Audio("assets/music.mp3")
+let audioTurn = new Audio("assets/ting.mp3")
+let gameover = new Audio("assets/gameover.mp3")
 
 let turn = "X";
 let isgameover = false;
@@ -12,67 +11,97 @@ let info = document.getElementsByClassName('info')
 let boxtext = document.getElementsByClassName('boxtext')
 
 // Change turns
- const changeTurn = ()=>{
-    return (turn === "X"?"0":"X")
- }
+const changeTurn = () => {
+    return (turn === "X" ? "0" : "X")
+}
 
 //  Check for a win
- const checkWin=()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName("boxtext")
-    let wins=[
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ]
-    wins.forEach((e)=>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== '')){
+    wins.forEach((e) => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== '')) {
             document.querySelector('.info').innerText = `${boxtext[e[0]].innerText} Won`
             gameover.play()
             music.play()
-            document.getElementsByTagName('img')[0].style.width='90px'
+            document.getElementById('gif').style.width = '90px'
             isgameover = true
-            Array.from(boxes).forEach((element)=>{
-    let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click',(e)=>{
-        if(boxtext.innerText === ''){
-            boxtext.innerText = '-';
-        }
-    })
-})
+            Array.from(boxes).forEach((element) => {
+                let boxtext = element.querySelector('.boxtext');
+                element.addEventListener('click', (e) => {
+                    if (boxtext.innerText === '') {
+                        boxtext.innerText = '-';
+                    }
+                })
+            })
 
         }
     })
- }
+}
 
 //  Game logic
-Array.from(boxes).forEach((element)=>{
+Array.from(boxes).forEach((element) => {
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click',(e)=>{
-        if(boxtext.innerText === '' && (isgameover != true)){
+    element.addEventListener('click', (e) => {
+        if (boxtext.innerText === '' && (isgameover != true)) {
             boxtext.innerText = turn;
             turn = changeTurn()
             audioTurn.play()
             checkWin()
-            if(!isgameover){
+            if (!isgameover) {
                 info[0].innerText = `Turn for ${turn}`
             }
         }
     })
 })
 
-btn.addEventListener('click',()=>{
-    Array.from(boxtext).forEach((e)=>{
+btn.addEventListener('click', () => {
+    Array.from(boxtext).forEach((e) => {
         e.innerText = ""
     })
-    turn="X"
+    turn = "X"
     isgameover = false
     info[0].innerText = `Turn for ${turn}`
     document.querySelector('.info').innerText = ``
-    document.getElementsByTagName('img')[0].style.width='0px'
+    document.getElementById('gif').style.width = '0px'
     music.pause()
 })
+
+let toggleBtn = document.querySelector(".btn")
+let light = true;
+toggleBtn.addEventListener('click', () => {
+    if (light) {
+        document.body.classList.remove('light-mode')
+        document.body.classList.add('dark-mode')
+        Array.from(boxes).forEach((box) => {
+            box.style.borderColor = 'white'; // Set border color for dark mode
+        });
+        toggleBtn.style.backgroundColor = 'white'; // Set background color for dark mode
+        toggleBtn.style.color = 'black'; // Set background color for dark mode
+        document.getElementById("mode").innerText = "Light";
+        light = !light;
+    }
+    else {
+        document.body.classList.remove('dark-mode')
+        document.body.classList.add('light-mode')
+        Array.from(boxes).forEach((box) => {
+            box.style.borderColor = 'black'; // Set border color for dark mode
+        });
+        toggleBtn.style.backgroundColor = 'black'; // Set background color for dark mode
+        toggleBtn.style.color = 'white'; // Set background color for dark mode
+        document.getElementById("mode").innerText = "Dark";
+        light = true;
+
+    }
+})
+
+
